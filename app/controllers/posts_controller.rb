f08bd1c929a069c2e1c273a73b23f38fd589c8f7
Brett Sleyster <brett.sleyster@kellogg.northwestern.edu>
2023-05-19 13:14:40 +0000

    post begin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,24 @@
 class PostsController < ApplicationController
+  class PostsController < ApplicationController
+
+    def index
+      @posts = Post.all
+    end
+  
+    def new
+      @post = Post.new
+    end
+  
+    def create
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["posted_on"] = params["post"]["place_id"]
+      @post.save
+      redirect_to "/posts"
+    end
+  
+  end
+
 end
